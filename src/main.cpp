4d57fb931d0d5b90311a6da3d694a05607f6815d
khlorz <lightningryousei@gmail.com>
2026-02-07 23:51:53 +0800

    Added imgui library
    Improved main loop setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,153 @@
+/****************************************************
+* Fusion Calculator's main loop
+****************************************************/
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_sdl3.h"
+#include "imgui/imgui_impl_sdlrenderer3.h"
+
 #include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
 
+#include <utility>
 #include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <cstring>
 
-int main()
+static float global_scaling = 1.0f;
+
+static void InitSDL(SDL_Window*& window, SDL_Renderer*& renderer);
+static void InitImGui(SDL_Window* window, SDL_Renderer* renderer);
+
+int main(int, char**)
 {
-	SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
 
-	SDL_Window* window = 
-		SDL_CreateWindow(
-			"Fusion Calculator",
-			1280,
-			720,
-			SDL_WINDOW_HIGH_PIXEL_DENSITY
-		);
+    try
+    {
+        InitSDL(window, renderer);
+        InitImGui(window, renderer);
 
-	if (window == nullptr)
-	{
-		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+        ImGuiIO& io = ImGui::GetIO();
+        bool show_demo_window = true;
+        ImVec4 clear_color = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
 
-	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
-	SDL_SetHint(SDL_HINT_RENDER_GPU_LOW_POWER, "1");
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-	if (renderer == nullptr) {
-		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
+        bool done = false;
+        while (!done)
+        {
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
+            {
+                ImGui_ImplSDL3_ProcessEvent(&event);
+                if (event.type == SDL_EVENT_QUIT)
+                    done = true;
+                if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
+                    done = true;
+            }
 
-	SDL_Event e;
-	bool quit = false;
+            if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
+            {
+                SDL_Delay(10);
+                continue;
+            }
 
-	// Define a rectangle
-	SDL_FRect greenSquare{ 270, 190, 100, 100 };
+            ImGui_ImplSDLRenderer3_NewFrame();
+            ImGui_ImplSDL3_NewFrame();
+            ImGui::NewFrame();
 
-	while (!quit) {
-		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_EVENT_QUIT) {
-				quit = true;
-			}
-		}
+            ImGui::ShowDemoWindow(&show_demo_window);
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
-		SDL_RenderClear(renderer); // Clear the renderer
+            ImGui::Render();
+            SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+            SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+            SDL_RenderClear(renderer);
+            ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+            SDL_RenderPresent(renderer);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Something unknown happened\n";
+    }
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Set render draw color to green
-		SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+    if (ImGui::GetCurrentContext() != nullptr)
+    {
+        ImGui_ImplSDLRenderer3_Shutdown();
+        ImGui_ImplSDL3_Shutdown();
+        ImGui::DestroyContext();
+    }
 
-		SDL_RenderPresent(renderer); // Render the screen
-	}
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+    }
+    SDL_Quit();
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
+
+void InitSDL(SDL_Window*& window, SDL_Renderer*& renderer)
+{
+    constexpr const char* window_name = "Fusion Calculator";
+    constexpr int window_width = 1280;
+    constexpr int window_height = 720;
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        char buf[128];
+        sprintf_s(buf, "Error: SDL_Init(): %s\n", SDL_GetError());
+        throw std::runtime_error(buf);
+    }
+
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    window = SDL_CreateWindow(window_name, (int)(window_width * main_scale), (int)(window_height * main_scale), window_flags);
+    if (window == nullptr)
+    {
+        char buf[128];
+        sprintf_s(buf, "Error: SDL_Init(): %s\n", SDL_GetError());
+        throw std::runtime_error(buf);
+    }
+
+    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (renderer == nullptr)
+    {
+        char buf[128];
+        sprintf_s(buf, "Error: SDL_Init(): %s\n", SDL_GetError());
+        throw std::runtime_error(buf);
+    }
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+}
+
+void InitImGui(SDL_Window* window, SDL_Renderer* renderer)
+{
+    if (ImGui::CreateContext() == nullptr)
+    {
+        throw std::runtime_error("ImGui not initialized!");
+    }
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.IniFilename = nullptr;
+
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(global_scaling);
+    style.FontScaleDpi = global_scaling;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+}
+
